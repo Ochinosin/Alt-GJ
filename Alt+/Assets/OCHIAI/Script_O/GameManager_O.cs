a1e513b8691a48157b26f0ff381ff5ee5f12a2b8
Ochinosin <m0113099c0@edu.teu.ac.jp>
2016-02-20 17:27:02 +0900

    更新しました

---
+++ Alt+/Assets/OCHIAI/Script_O/GameManager_O.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager_O : MonoBehaviour {
+	[SerializeField] private int state = 0;
+	[SerializeField] private Image clear;
+	[SerializeField] private Animator anim;
+	[SerializeField] private string BoolName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			state = 1;
+		}
+		if (state == 1) {
+			clear.enabled = true;
+			anim.SetBool (BoolName, true);
+		}
+	}
+}
