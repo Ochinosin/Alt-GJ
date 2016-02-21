8a2025c367e9575ffedba40ba4001e9cd32714ca
Ochinosin <m0113099c0@edu.teu.ac.jp>
2016-02-21 09:52:08 +0900

    色々追加しました

---
+++ Alt+/Assets/OCHIAI/Script_O/GUICtrl_O.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUICtrl_O : MonoBehaviour {
+	public Slider hp;
+	public Text timeGUI;
+	public Text helpGUI;
+	public float time = 13f;
+	private bool help; //お助け可能かどうか
+
+	// Use this for initialization
+	void Start () {
+		//hp.value=100;
+		timeGUI.text = time.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(time<10f) help = true; // お助け判定関数ができたら変更
+
+		// helpの値によってお助け可か判定
+		if (help) {
+			helpGUI.color = Color.red;
+			helpGUI.text = "お助け\n可能!";
+		} else {
+			helpGUI.color = Color.green;
+			helpGUI.text = "お助け\n準備中";
+		}
+
+		//		hp.value--; // プレイヤーobjできたら変更
+
+		if (time < 10f) {
+			timeGUI.color = Color.red;
+			/*float alpha = timeGUI.color.a;
+			if (alpha == 1.0f)
+				timeGUI.color = new Color (timeGUI.color.r, timeGUI.color.g, timeGUI.color.b, 0.0f);
+			else
+				timeGUI.color = new Color (timeGUI.color.r, timeGUI.color.g, timeGUI.color.b, 1.0f);*/
+		}
+
+		// 残り時間表示
+		if(time>0) time-=1f*Time.deltaTime;
+		timeGUI.text = "Time:"+((int)time).ToString ()+"sec";	
+	}
+}
--- Alt+/Assets/OCHIAI/Script_O/GameManager_O.cs
+++ Alt+/Assets/OCHIAI/Script_O/GameManager_O.cs
@@ -10,43 +10,105 @@ public class GameManager_O : MonoBehaviour {
 	[SerializeField] private Image over;
 	[SerializeField] private Animator anim2;
 	[SerializeField] private string BoolName2;
+	[SerializeField] private GameObject start;
+	[SerializeField] private Animator anim3;
+	[SerializeField] private string BoolName3;
+	[SerializeField] private Image player;
+	[SerializeField] private Animator anim4;
+	[SerializeField] private string BoolName4;
+	[SerializeField] private Text time;
+	[SerializeField] private Text help;
+	[SerializeField] private bool endSart;
+	[SerializeField] private string TopTitle;
+	[SerializeField] private Image black;
 	[SerializeField] private int col;
+	[SerializeField] private AudioSource BGM;
+	[SerializeField] private AudioSource SE;
+	[SerializeField] private AudioClip gameOver;
+	[SerializeField] private AudioClip gameClear;
 
 	public GameObject Player;
+	public GameObject Canvas;
+	private bool OV;
+	private bool CL;
 
 	// Use this for initialization
 	void Start () {
-	
+		anim3.SetBool (BoolName3, true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		/*if (Input.GetKeyDown (KeyCode.Space)) {
 			state = 1;
 		}
 		if (Input.GetKeyDown (KeyCode.A)) {
 			state = 2;
+		}*/
+		if (state == 0) {
+			BGM.volume += 0.002f;
+			if (BGM.volume >= 0.2f) {
+				BGM.volume = 0.2f;
+			}
 		}
+
 		if (state == 1) {
+			BGM.volume = 0.0f;
+			if (CL == false) {
+				SE.PlayOneShot (gameClear);
+				CL = true;
+			}
 			clear.enabled = true;
 			over.enabled = false;
+			GetComponent<GUICtrl_O> ().enabled = false;
 			anim.SetBool (BoolName, true);
 		}
 		if (state == 2) {
+			BGM.volume = 0.0f;
+			Player.GetComponent<Player_O> ().enabled = false;
+			time.enabled = false;
+			help.enabled = false;
 			clear.enabled = false;
 			over.enabled = true;
 			anim2.SetBool (BoolName2, true);
-		}
-
-		if (col == 0) {
-			Time.timeScale = 0;
-			if (Input.GetKeyDown (KeyCode.Q)) {
+			if (Input.GetKeyDown (KeyCode.Return)) {
 				col = 1;
 			}
 		}
+		if (col == 0) {
+			black.color -= new Color (0f, 0f, 0f, 0.02f);
+			if(black.color.a <= 0f){
+				black.color += new Color (0f, 0f, 0f, 0.0f);
+			}
+		}
 		if (col == 1) {
-			Time.timeScale = 1;
-			Player.GetComponent<Player_O> ().enabled = true;
+			anim.speed = 0;
+			black.color += new Color (0f, 0f, 0f, 0.02f);
+			if(black.color.a >= 1f){
+				black.color += new Color (0f, 0f, 0f, 1.0f);
+				Application.LoadLevel (TopTitle);
+			}
+		}
+
+		if (Canvas.GetComponent<GUICtrl_O> ().time <= 1) {
+			if (OV == false) {
+				SE.PlayOneShot (gameOver);
+				OV = true;
+			}
+			state = 2;
+		}
+
+		if (endSart == false) {
+			AnimatorStateInfo animInfo;
+			animInfo = start.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0);
+			if (animInfo.normalizedTime >= 1.0f) {
+				Player.GetComponent<Player_O> ().enabled = true;
+				anim4.SetBool (BoolName4, true);
+				time.enabled = true;
+				help.enabled = true;
+				GetComponent<GUICtrl_O> ().enabled = true;
+				endSart = true;
+			}
 		}
 	}
 }
